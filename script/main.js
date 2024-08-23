00b15d91bd7ffa3c348b069041182e759ce48d1e
William Yang <77467499+wilyan09007@users.noreply.github.com>
2024-08-22 19:26:09 -0700

    s

--- script/main.js
+++ script/main.js
@@ -216,62 +216,6 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .from(
-      ".eric2", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric3", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric4", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric5", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric6", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric7", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
-    .from(
-      ".eric8", 0.5, {
-        scale: 1,
-        opacity: 0,
-        rotationZ: -45
-      },
-      "-=2"
-    )
     .from(".hat", 0.5, {
       x: -100,
       y: 350,
